c4f8de1e11ff704af4f3f53474b9e359ac0fd1d2
Dmitry Solonko <d.solonko@innopolis.ru>
2024-10-26 12:13:14 +0300

    изменён title в context предсваления index

--- TisbiProject/main/views.py
+++ TisbiProject/main/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context = {
-        "title": "Main index page",
+        "title": "Main index page1231231231232",
         "values": ["hello", "wOrLd", "123"]
     }
     return render(request, 'main/index.html', context)
